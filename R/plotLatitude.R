b6277bc5d9945be546a7a90a326f43b9c3eaa825
ChloeRNater <chloe.nater@nina.no>
2023-12-04 13:31:58 +0100

    Completed latitude plotting function

--- R/plotLatitude.R
+++ R/plotLatitude.R
@@ -1,190 +1,151 @@
-plotLatitude <- function(mcmc.out, 
-                         N_areas, area_names, area_coord, N_sites, 
-                         min_years, max_years, minYear, maxYear,
+#' Plot posterior summaries for vital rate and population parameters as a function of latitude
+#'
+#' @param PostSum.list a list containing 5 dataframes with summarised posteriors for several
+#' parameters by area: average recruitment (rRep.sum), average survival 
+#' (pSurv.sum), rodent effect slope (betaR.sum, = NA when fitRodentCov = FALSE), 
+#' average population density (popDens.sum), and population growth rate 
+#' (lambda.sum). 
+#' @param area_coord a tibble containing coordinate data for each area in analysis.
+#' @param minYear integer. First year in the analysis.
+#' @param maxYear integer. Last year in the analysis.
+#' @param fitRodentCov logical. If TRUE, makes plot for rodent effect slope in 
+#' addition to other plots. 
+#'
+#' @return a vector of pdf plot names. The plots can be found in Plots/Latitude.
+#' @export
+#'
+#' @examples
+
+plotLatitude <- function(PostSum.list, 
+                         area_coord,
+                         minYear, maxYear,
                          fitRodentCov){
   
-  
-  ## Convert posterior samples to matrix format
-  out.mat <- as.matrix(mcmc.out)
-  
-  ## Set year index ranges for plotting shared data collection period only
-  minYearIdx_shared <- max(min_years)
-  maxYearIdx_shared <- min(max_years)
-  
-  ## Determine area-specific year range
-  area_yearIdxs <- (1:(maxYear-minYear+1))
-  area_years <- area_yearIdxs + (minYear - 1)
-  
-  ## Calculate area- and year-specific population growth rates
-  for(i in 1:N_areas){
-    for(t in 1:length(area_years)){
-      
-      # Summarize annual average population densities (per area, averaged over lines)
-      popDens_juv_t0 <- out.mat[, paste0("Density[",  i, ", 1, ", 1:N_sites[i], ", ", area_yearIdxs[t], "]")]
-      popDens_ad_t0 <- out.mat[, paste0("Density[",  i, ", 2, ", 1:N_sites[i], ", ", area_yearIdxs[t], "]")]
-      popDens_mean_t0 <- rowMeans(popDens_juv_t0 + popDens_ad_t0)
-      
-      if(t < length(area_years)){
-        popDens_juv_t1 <- out.mat[, paste0("Density[",  i, ", 1, ", 1:N_sites[i], ", ", area_yearIdxs[t+1], "]")]
-        popDens_ad_t1 <- out.mat[, paste0("Density[",  i, ", 2, ", 1:N_sites[i], ", ", area_yearIdxs[t+1], "]")]
-        popDens_mean_t1 <- rowMeans(popDens_juv_t1 + popDens_ad_t1)
-        
-        # Calculate annual average population growth rate
-        lambda_t0t1 <- popDens_mean_t1 / popDens_mean_t0
-      }
-      
-      # Merge average density back into posterior samples
-      out.mat <- cbind(out.mat, popDens_mean_t0)
-      colnames(out.mat)[dim(out.mat)[2]] <- paste0("meanDens[",i, ", ", area_yearIdxs[t], "]" )
-      
-      
-      # Merge population growth rate back into posterior samples
-      if(t < length(area_years)){
-        out.mat <- cbind(out.mat, lambda_t0t1)
-        colnames(out.mat)[dim(out.mat)[2]] <- paste0("lambda[",i, ", ", area_yearIdxs[t], "]" )
-      }
-    }
-    
-    ## Calculate average area-specific average densities and population growth rate over different time periods
-    
-    # Entire time period - average density
-    lambda_mean_tot <- rowMeans(out.mat[, paste0("meanDens[", i, ", ", area_yearIdxs[1:(length(area_years)-1)], "]")])
-    out.mat <- cbind(out.mat, lambda_mean_tot)
-    colnames(out.mat)[dim(out.mat)[2]] <- paste0("densAvg_tot[", i, "]")
-    
-    # Overlapping time period (all areas) - average density
-    lambda_mean_shared <- rowMeans(out.mat[, paste0("meanDens[", i, ", ", minYearIdx_shared:(maxYearIdx_shared-1), "]")])
-    out.mat <- cbind(out.mat, lambda_mean_shared)
-    colnames(out.mat)[dim(out.mat)[2]] <- paste0("densAvg_shared[", i, "]")
-    
-    # Entire time period - population growth rate
-    lambda_mean_tot <- rowMeans(out.mat[, paste0("lambda[", i, ", ", area_yearIdxs[1:(length(area_years)-1)], "]")])
-    out.mat <- cbind(out.mat, lambda_mean_tot)
-    colnames(out.mat)[dim(out.mat)[2]] <- paste0("lambdaAvg_tot[", i, "]")
-    
-    # Overlapping time period (all areas) - population growth rate
-    lambda_mean_shared <- rowMeans(out.mat[, paste0("lambda[", i, ", ", minYearIdx_shared:(maxYearIdx_shared-1), "]")])
-    out.mat <- cbind(out.mat, lambda_mean_shared)
-    colnames(out.mat)[dim(out.mat)[2]] <- paste0("lambdaAvg_shared[", i, "]")
-  }
-  
-  ## Summarize posteriors for relevant parameters
-  
-  # Prepare matrices for storage of results
-  rRep.sum <- pSurv.sum <- betaR.sum <- data.frame()
-  popDens.sum <- lambda.sum <- data.frame()
-  
-  for(i in 1:N_areas){
-    
-    # Summarize average reproductive rates
-    rRep_name <- paste0("Mu.R[",  i, "]")
-    rRep_add <- data.frame(Area = area_names[i],
-                           Median = median(out.mat[, rRep_name]),
-                           lCI = unname(quantile(out.mat[, rRep_name], probs = 0.025)),
-                           uCI = unname(quantile(out.mat[, rRep_name], probs = 0.975)),
-                           Mean = mean(out.mat[, rRep_name]),
-                           SD = sd(out.mat[, rRep_name])
-    )
-    rRep.sum <- rbind(rRep.sum, rRep_add)
-    
-    # Summarize average annual survival rates
-    pSurv_name <- paste0("Mu.S[",  i, "]")
-    pSurv_add <- data.frame(Area = area_names[i],
-                            Median = median(out.mat[, pSurv_name]),
-                            lCI = unname(quantile(out.mat[, pSurv_name], probs = 0.025)),
-                            uCI = unname(quantile(out.mat[, pSurv_name], probs = 0.975)),
-                            Mean = mean(out.mat[, pSurv_name]),
-                            SD = sd(out.mat[, pSurv_name])
-    )
-    pSurv.sum <- rbind(pSurv.sum, pSurv_add)
-    
-    # Summarize rodent effects
-    if(fitRodentCov){
-      betaR_name <- paste0("betaR.R[",  i, "]")
-      betaR_add <- data.frame(Area = area_names[i],
-                              Median = median(out.mat[, betaR_name]),
-                              lCI = unname(quantile(out.mat[, betaR_name], probs = 0.025)),
-                              uCI = unname(quantile(out.mat[, betaR_name], probs = 0.975)),
-                              Mean = mean(out.mat[, betaR_name]),
-                              SD = sd(out.mat[, betaR_name])
-      )
-      betaR.sum <- rbind(betaR.sum, betaR_add)
-    }
-    
-    
-    # Summarize average population densities
-    Dens_names <- c(paste0("densAvg_tot[",  i, "]"), paste0("densAvg_shared[",  i, "]"))
-    Dens_tot_add <- data.frame(Area = area_names[i],
-                               SummaryPeriod = paste0(minYear, "-", maxYear),
-                               Median = median(out.mat[, Dens_names[1]]),
-                               lCI = unname(quantile(out.mat[, Dens_names[1]], probs = 0.025)),
-                               uCI = unname(quantile(out.mat[, Dens_names[1]], probs = 0.975)),
-                               Mean = mean(out.mat[, Dens_names[1]]),
-                               SD = sd(out.mat[, Dens_names[1]])
-    )
-    Dens_shared_add <- data.frame(Area = area_names[i],
-                                  SummaryPeriod = paste0(minYearIdx_shared + minYear - 1, "-", maxYearIdx_shared + minYear - 1),
-                                  Median = median(out.mat[, Dens_names[2]]),
-                                  lCI = unname(quantile(out.mat[, Dens_names[2]], probs = 0.025)),
-                                  uCI = unname(quantile(out.mat[, Dens_names[2]], probs = 0.975)),
-                                  Mean = mean(out.mat[, Dens_names[2]]),
-                                  SD = sd(out.mat[, Dens_names[2]])
-    )
-    popDens.sum <- rbind(popDens.sum, Dens_tot_add, Dens_shared_add)
-    
-    # Summarize average population growth rates
-    lambda_names <- c(paste0("lambdaAvg_tot[",  i, "]"), paste0("lambdaAvg_shared[",  i, "]"))
-    lambda_tot_add <- data.frame(Area = area_names[i],
-                                 SummaryPeriod = paste0(minYear, "-", maxYear),
-                                 Median = median(out.mat[, lambda_names[1]]),
-                                 lCI = unname(quantile(out.mat[, lambda_names[1]], probs = 0.025)),
-                                 uCI = unname(quantile(out.mat[, lambda_names[1]], probs = 0.975)),
-                                 Mean = mean(out.mat[, lambda_names[1]]),
-                                 SD = sd(out.mat[, lambda_names[1]])
-    )
-    lambda_shared_add <- data.frame(Area = area_names[i],
-                                    SummaryPeriod = paste0(minYearIdx_shared + minYear - 1, "-", maxYearIdx_shared + minYear - 1),
-                                    Median = median(out.mat[, lambda_names[2]]),
-                                    lCI = unname(quantile(out.mat[, lambda_names[2]], probs = 0.025)),
-                                    uCI = unname(quantile(out.mat[, lambda_names[2]], probs = 0.975)),
-                                    Mean = mean(out.mat[, lambda_names[2]]),
-                                    SD = sd(out.mat[, lambda_names[2]])
-    )
-    lambda.sum <- rbind(lambda.sum, lambda_tot_add, lambda_shared_add)
-  } 
-  
-  ## Add coordinate information to summarized posterior data
+  ## Add coordinate information to summarized posterior data and plot
   area_coord <- area_coord %>%
-    dplyr::rename(Area = spatialUnit)
+    dplyr::rename(Area = spatialUnit,
+                  Longitude = longitudeAvg,
+                  Latitude = latitudeAvg)
   
   # Average reproductive rates
-  rRep.sum <- rRep.sum %>%
+  rRep.sum <- PostSum.list$rRep.sum %>%
     dplyr::left_join(area_coord, by = "Area")
   
+  p_rRep <- ggplot(rRep.sum) + 
+    geom_pointrange(aes(x = Latitude, y = Median, ymin = lCI, ymax = uCI, colour = Longitude), size = 0.5, fatten = 4, alpha = 0.5) +
+    paletteer::scale_color_paletteer_c("grDevices::Temps") + 
+    ylab("Estimate") + 
+    theme_classic()
+  
   # Average annual survival 
-  pSurv.sum <- pSurv.sum %>%
+  pSurv.sum <- PostSum.list$pSurv.sum %>%
     dplyr::left_join(area_coord, by = "Area")
+  
+  p_pSurv <- ggplot(pSurv.sum) + 
+    geom_pointrange(aes(x = Latitude, y = Median, ymin = lCI, ymax = uCI, colour = Longitude), size = 0.5, fatten = 4, alpha = 0.5) +
+    paletteer::scale_color_paletteer_c("grDevices::Temps") + 
+    ylab("Estimate") + 
+    theme_classic()
   
   # Rodent effects 
   if(fitRodentCov){
-    betaR.sum <- betaR.sum %>%
+    betaR.sum <- PostSum.list$betaR.sum %>%
       dplyr::left_join(area_coord, by = "Area")
+    
+    p_betaR <- ggplot(betaR.sum) + 
+      geom_pointrange(aes(x = Latitude, y = Median, ymin = lCI, ymax = uCI, colour = Longitude), size = 0.5, fatten = 4, alpha = 0.5) +
+      ylab("Estimate") + 
+      paletteer::scale_color_paletteer_c("grDevices::Temps") + 
+      theme_classic()
   }
   
   # Average population densities
-  popDens.sum <- popDens.sum %>%
+  popDens.sum <- PostSum.list$popDens.sum %>%
     dplyr::left_join(area_coord, by = "Area")
   
-  # Average population growth rates
-  lambda.sum <- lambda.sum %>%
-    dplyr::left_join(area_coord, by = "Area")
-  
-  
-  ## Plot estimates as a function of longitude (and latitude)
-  ggplot(rRep.sum) + 
-    geom_pointrange(aes(x = latitudeAvg, y = Median, ymin = lCI, ymax = uCI, colour = longitudeAvg), size = 0.3, fatten = 4) +
+  p_popDens1 <- ggplot(subset(popDens.sum, SummaryPeriod == paste0(minYear, "-", maxYear))) + 
+    geom_pointrange(aes(x = Latitude, y = Median, ymin = lCI, ymax = uCI, colour = Longitude), size = 0.5, fatten = 4, alpha = 0.5) +
+    ylab("Estimate") + 
     paletteer::scale_color_paletteer_c("grDevices::Temps") + 
     theme_classic()
   
+  p_popDens2 <- ggplot(subset(popDens.sum, SummaryPeriod != paste0(minYear, "-", maxYear))) + 
+    geom_pointrange(aes(x = Latitude, y = Median, ymin = lCI, ymax = uCI, colour = Longitude), size = 0.5, fatten = 4, alpha = 0.5) +
+    ylab("Estimate") + 
+    paletteer::scale_color_paletteer_c("grDevices::Temps") + 
+    theme_classic()
+  
+  # Average population growth rates
+  lambda.sum <- PostSum.list$lambda.sum %>%
+    dplyr::left_join(area_coord, by = "Area")
+  
+  p_lambda1 <- ggplot(subset(lambda.sum, SummaryPeriod == paste0(minYear, "-", maxYear))) + 
+    geom_pointrange(aes(x = Latitude, y = Median, ymin = lCI, ymax = uCI, colour = Longitude), size = 0.5, fatten = 4, alpha = 0.5) +
+    ylab("Estimate") + 
+    paletteer::scale_color_paletteer_c("grDevices::Temps") + 
+    theme_classic()
+  
+  p_lambda2 <- ggplot(subset(lambda.sum, SummaryPeriod != paste0(minYear, "-", maxYear))) + 
+    geom_pointrange(aes(x = Latitude, y = Median, ymin = lCI, ymax = uCI, colour = Longitude), size = 0.5, fatten = 4, alpha = 0.5) +
+    ylab("Estimate") + 
+    paletteer::scale_color_paletteer_c("grDevices::Temps") + 
+    theme_classic()
+  
+  
+  ## Make plotting directory if it does not exist yet
+  ifelse(!dir.exists("Plots/Latitude"), dir.create("Plots/Latitude"), FALSE)
+  
+  ## Assemble plots (vital rate parameters)
+  if(fitRodentCov){
+    pdf("Plots/Latitude/VitalRateParams_Latitude.pdf", width = 8, height = 6)
+    print(
+      ggpubr::ggarrange(p_rRep + ggtitle("Average recruitment rate") + theme(axis.title.x = element_blank()), 
+                        p_pSurv + ggtitle("Average survival rate") + theme(axis.title.x = element_blank()),  
+                        p_betaR + ggtitle("Rodent effect on recruitment"),
+                        heights = c(1, 1, 1.2), ncol = 1, 
+                        legend = "right", common.legend = TRUE)
+    )
+    dev.off()
+  }else{
+    pdf("Plots/Latitude/VitalRateParams_Latitude.pdf", width = 8, height = 4)
+    print(
+      ggpubr::ggarrange(p_rRep + ggtitle("Average recruitment rate") + theme(axis.title.x = element_blank()), 
+                        p_pSurv + ggtitle("Average survival rate"),  
+                        heights = c(1, 1.2), ncol = 1, 
+                        legend = "right", common.legend = TRUE)
+    )
+    dev.off()
+  }
+
+  
+  
+  ## Assemble plots (population-level parameters)
+  pdf("Plots/Latitude/PopParams1_Latitude.pdf", width = 8, height = 4)
+  print(
+    ggpubr::ggarrange(p_popDens1 + ggtitle(paste0("Average population density (", minYear, "-", maxYear, ")")) + theme(axis.title.x = element_blank()),
+                      p_lambda1 + ggtitle(paste0("Average population growth rate (", minYear, "-", maxYear, ")")),
+                      heights = c(1, 1.1), ncol = 1, 
+                      legend = "right", common.legend = TRUE)
+  )
+  dev.off()
+  
+  
+  shared_sumPeriod <- subset(popDens.sum, SummaryPeriod != paste0(minYear, "-", maxYear))$SummaryPeriod[1]
+  
+  pdf("Plots/Latitude/PopParams2_Latitude.pdf", width = 8, height = 4)
+  print(ggpubr::ggarrange(p_popDens2 + ggtitle(paste0("Average population density (", shared_sumPeriod, ")")) + theme(axis.title.x = element_blank()),
+                          p_lambda2 + ggtitle(paste0("Average population growth rate (", shared_sumPeriod, ")")),
+                          heights = c(1, 1.1), ncol = 1, 
+                          legend = "right", common.legend = TRUE)
+  )
+  dev.off()
+  
+  
+  ## Write and return plot paths
+  plot.paths <- c("Plots/Latitude/VitalRateParams_Latitude.pdf", 
+                  "Plots/Latitude/PopParams1_Latitude.pdf",
+                  "Plots/Latitude/PopParams2_Latitude.pdf")
+  
+  return(plot.paths)
   
 }
