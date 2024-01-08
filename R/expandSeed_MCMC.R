8c18d8435f985d7c77ef59d44eecd8f9f374bb5d
ChloeRNater <chloe.nater@nina.no>
2024-01-08 10:52:55 +0100

    Updated iseed expansion

--- R/expandSeed_MCMC.R
+++ R/expandSeed_MCMC.R
@@ -1,16 +1,15 @@
 expandSeed_MCMC <- function(seed, nchains){
   
-  if(nchains > 6){
-    stop("Seed expansion is currently only supported for up to 6 chains.")
+  if(nchains > 5){
+    stop("Seed expansion is currently only supported for up to 5 chains.")
   }
   
   seed.list <- c(
     seed,
     round((seed*70)/4),
-    round(seed/3)+1,
-    seed + 88,
+    (seed + 10)*7,
     round(seed*5/3),
-    seed + 37
+    seed + 37*2
   )
   
   return(seed.list[1:nchains])
